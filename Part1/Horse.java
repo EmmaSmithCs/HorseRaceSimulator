b2ebb5b6c2b9a20d81f243abb610bc97e9008c3c
Emma Smith <smithemma.cs@gmail.com>
2024-04-22 16:11:08 +0100

    Updated the Horse class to have 5 fields, a constructor, the relevant methods witht the whole class encapsualted properly

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -2,13 +2,17 @@
 /**
  * Write a description of class Horse here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Emma Smith) 
+ * @version (20/04/2024)
  */
 public class Horse
 {
     //Fields of class Horse
-    
+    private String horseName;
+    private char horseSymbol;
+    private int distanceTravelled;
+    private boolean fallen;
+    private double horseConfidence;
     
       
     //Constructor of class Horse
@@ -17,7 +21,11 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+        this.horseSymbol = horseSymbol;
+        this.horseName = horseName;
+        this.horseConfidence = horseConfidence;
+        this.distanceTravelled = 0; 
+        this.fallen = false;
     }
     
     
@@ -25,52 +33,52 @@ public class Horse
     //Other methods of class Horse
     public void fall()
     {
-        
+        this.fallen = true;
     }
     
     public double getConfidence()
     {
-        
+        return horseConfidence;
     }
     
     public int getDistanceTravelled()
     {
-        
+        return distanceTravelled;
     }
     
     public String getName()
     {
-        
+        return horseName;
     }
     
     public char getSymbol()
     {
-        
+        return horseSymbol; 
     }
     
     public void goBackToStart()
     {
-        
+        this.distanceTravelled = 0;
     }
     
     public boolean hasFallen()
     {
-        
+        return fallen;
     }
 
     public void moveForward()
     {
-        
+        this.distanceTravelled++;   
     }
 
     public void setConfidence(double newConfidence)
     {
-        
+        this.horseConfidence = newConfidence;
     }
     
     public void setSymbol(char newSymbol)
     {
-        
+        this.horseSymbol = newSymbol;
     }
     
 }
